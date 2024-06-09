d17f8055e2213709da1206bb15f8325ca5629f87
amirtanazzoh <92968872+amirtanazzoh@users.noreply.github.com>
2024-06-09 10:32:28 +0330

    1.0.1

--- inc/functions/cyn-update-checker.php
+++ inc/functions/cyn-update-checker.php
@@ -1,5 +1,5 @@
 <?php
-require( get_stylesheet_directory() . '/inc/libs/plugin-update-checker/plugin-update-checker.php' );
+require ( get_stylesheet_directory() . '/inc/libs/plugin-update-checker/plugin-update-checker.php' );
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $myUpdateChecker = PucFactory::buildUpdateChecker(
@@ -12,4 +12,4 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 $myUpdateChecker->setBranch( 'amirtanazzoh' );
 
 //Optional: If you're using a private repository, specify the access token like this:
-$myUpdateChecker->setAuthentication( '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' );
+// $myUpdateChecker->setAuthentication( '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' );
